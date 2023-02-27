44855b746540be90685d9a8faa0c82221c912a1f
Tushar Bari <97783152+TusharBari2001@users.noreply.github.com>
2023-02-27 16:05:02 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.title('Tushar Bari')
+st.header(':white[Innomatics Research labs]')
+st.subheader(":white [Data Science Internship]")
+
+btn_click = st.button("Tap Me ")
+if btn_click == True:
+    st.subheader("Hello There :)")
+    st.caption('')
+
+    st.subheader('Join me on LinkedIN and GitHub 👇')
+
+    st.markdown('GitHub: https://github.com/TusharBari2001/')
+    st.markdown('Linkedin : https://www.linkedin.com/in/tushar-bari-539363244/')
